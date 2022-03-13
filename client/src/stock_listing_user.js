0a08d4b60f81cae1a877f4092041573c8b5dc181
Mawil Hasan <mawil0721@berkeley.edu>
2022-03-12 17:24:05 -0800

    started hard coding css. ran into bugs with containers and fixed. finished topContainer, but needs to be changed to match the figma. started the bottomContaienr css

--- client/src/stock_listing_user.js
+++ client/src/stock_listing_user.js
@@ -1,42 +1,46 @@
 import React from "react";
 import './stock_listing_user.css';
 
+
 function StockListingUser() {
     return (
-        <div className = "stockListingPage">
-            <div className = "topContainer"> 
-                <div className = "leftSide">
-                    <h1>Edit Today's Stock</h1>
-                    <h2>Manage today's stock listing.</h2>
-                    
-                    
+        <div className= "entireContent">
+            <div className = "stockListingPage">
+                <div className = "topContainer"> 
+                    <div className = "leftSide">
+                    {/* style ={{textAlign : 'left', color: 'blue'}} */}
+                        <h1>Edit Today's Stock</h1>
+                        <p>Manage today's stock listing.</p>
+                        
+                        
+                    </div>
+                    <div className = "rightSide">
+                        {/* <button> +   Add New Item </button> */}
+                        <button type = "button" class = "btn">+   Add Item</button>
+                        <h3>Set all items to out of stock</h3>
+                        
+                        
+                    </div>
+                
+                
                 </div>
-                <div className = "rightSide">
-                    <button> Add New Item </button>
-                    <h2>Set all items to out of stock</h2>
-                    
-                    
-                </div>
-            
-            
-            </div>
-            
-            <div className = "bottomContainer">
-                <div className = "filterItemControls">
-                    <h1>Filter Items</h1>
-                    <h2>Reset Filters</h2>
-                    <h3>Search Items</h3>
-                    <h4>Filter by Dietary Categories</h4>
-                    <h5>Sort by...</h5>
-                    <h6>Only </h6>
+                
+                <div className = "bottomContainer">
+                    <div className = "filterItemControls">
+                        <div className = ""></div>
+                        <h1>Filter Items</h1>
+                        <h2>Reset Filters</h2>
+                        <h3>Search Items</h3>
+                        <h4>Filter by Dietary Categories</h4>
+                        <h5>Sort by...</h5>
+                        <h6>Only </h6>
 
-                </div>
-                <div className = "filterItemDisplay">
-                    
+                    </div>
+                    <div className = "filterItemDisplay">
+                        
+                    </div>
                 </div>
             </div>
-
-        
         </div>
     )
 }
