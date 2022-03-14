b29c7dd41853047112b1984bf82cc3eb79e1e0e9
Anthony Lu <anthonylu@berkeley.edu>
2022-03-13 21:24:46 -0700

    test

---
+++ client/src/navbar.js
@@ -0,0 +1,13 @@
+export default function AdminLogin() {
+     return(
+      <div className = 'adminComponentContainer'>
+          
+          <div className = 'adminLoginTextContainer'>
+              <div><h1>Login</h1>    
+              <p>Please sign in with an administrative account to <br></br>access additional features.</p>
+              </div>
+          </div>
+      </div>
+          
+     )
+  }
