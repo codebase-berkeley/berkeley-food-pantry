7504dca5dcb6decfbb8f5955cef8eb7a8b50a53c
Gargi Deshpande <gargi@berkeley.edu>
2022-03-13 21:01:21 -0700

    added defualt dropdowns and styled top

--- client/src/stock_listing_user.js
+++ client/src/stock_listing_user.js
@@ -1,5 +1,10 @@
 import React from "react";
 import './stock_listing_user.css';
+import {PanelGroup, Panel} from 'rsuite';
+import 'rsuite/dist/rsuite.min.css';
+import {Dropdown} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import isbesPlusSign from './plusSign.svg';
 
 
 function StockListingUser() {
@@ -16,9 +21,11 @@ function StockListingUser() {
                     </div>
                     <div className = "rightSide">
                         {/* <button> +   Add New Item </button> */}
-                        <button type = "button" class = "btn">+   Add Item</button>
-                        <h3>Set all items to out of stock</h3>
-                        
+                        <button type = "button" class = "btn">
+                            <text> + </text>
+                            <text> Add New Item </text>
+                            </button>
+                        <div className="changeStock"> Set all items to out of stock </div>
                         
                     </div>
                 
@@ -27,17 +34,53 @@ function StockListingUser() {
                 
                 <div className = "bottomContainer">
                     <div className = "filterItemControls">
-                        <div className = ""></div>
-                        <h1>Filter Items</h1>
+                        {/* Needs to be flex: row side */}
+                        <h1>filter items</h1>
                         <h2>Reset Filters</h2>
-                        <h3>Search Items</h3>
-                        <h4>Filter by Dietary Categories</h4>
-                        <h5>Sort by...</h5>
+                       
+                        <Dropdown>
+                            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                Filter by Dietary Categories
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu>
+                                <Dropdown.Item>Another action</Dropdown.Item>
+                                <Dropdown.Item>Something else</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        
+                        <Dropdown>
+                            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                Sort By
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu>
+                                <Dropdown.Item>Another action</Dropdown.Item>
+                                <Dropdown.Item>Something else</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        
                         <h6>Only </h6>
 
+
+                        {/* Show header
+                            - 3 buttons (import package)
+                        */}
+                        
                     </div>
                     <div className = "filterItemDisplay">
-                        
+                        <Panel className = "foodPanel" shaded>fake food 1</Panel>
+                        <Panel className = "foodPanel" shaded>fake food 2</Panel>
+                        <Panel className = "foodPanel" shaded>fake food 3</Panel>
+                        <Panel className = "foodPanel" shaded>fake food 4</Panel>
+                        <Panel className = "foodPanel" shaded>fake food 1</Panel>
+                        <Panel className = "foodPanel" shaded>fake food 2</Panel>
+                        <Panel className = "foodPanel" shaded>fake food 3</Panel>
+                        <Panel className = "foodPanel" shaded>fake food 4</Panel>
+                        <Panel className = "foodPanel" shaded>fake food 1</Panel>
+                        <Panel className = "foodPanel" shaded>fake food 2</Panel>
+                        <Panel className = "foodPanel" shaded>fake food 3</Panel>
+                        <Panel className = "foodPanel" shaded>fake food 4</Panel>
                     </div>
                 </div>
             </div>
