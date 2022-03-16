7208b35267092dbd23d40527a2fc1a3c952dacc3
Anthony Lu <anthonylu@berkeley.edu>
2022-03-15 20:57:20 -0700

    add item done, however need to fix spacing between dropdown menus

--- client/src/AddFood.js
+++ client/src/AddFood.js
@@ -1,5 +1,6 @@
 import './AddFood.css';
 import saveitembutton from './assets/saveitembutton.png';
+import importimage from './assets/importimage.png';
 
 export default function AddFood() {
     return(
@@ -16,14 +17,27 @@ export default function AddFood() {
                             Item Name 
                             <input type="text" name="name" />
                          </label>
-        
+                         <label for="stock-select-list">Set Stock Availability</label>
+                            <select className="stock-select-list" id="selectList"> 
+                                <option value="option 1">In Stock Today</option>
+                                <option value="option 2">Not in Stock</option>
+                            </select>
+                        <label for="dietary-categories-select-list">Select Dietary Categories (Optional)</label>
+                            <select className="dietary-categories-select-list" id="selectList">
+                                <option value="option 1">Vegetarian</option>
+                                <option value="option 2">Vegan</option>
+                                <option value="option 3">None</option>
+                            </select>
                     </form>
                     </div>
-                    <div className = 'upload-image'>Upload image</div>
+                    <div className = 'upload-image'> 
+                        <div className = 'upload-image-text'>Upload image (Optional)</div>
+                            <img src ={importimage} className = "import-image-button" alt = "save item button"></img>      
+                    </div>
                 </div>
-                <div className = 'save-item-button-container'>
-                    <img src ={saveitembutton} className = "save-item-button" alt = "save item button"></img>
-                </div>
+            <div className = 'save-item-button-container'>
+                <img src ={saveitembutton} className = "save-item-button" alt = "save item button"></img>
+            </div>
         </div>
     )
 }
