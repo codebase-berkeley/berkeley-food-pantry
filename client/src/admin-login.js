5fb4d6e5f6a0253e4e38caf685e7f03c41528d57
Anthony Lu <anthonylu@berkeley.edu>
2022-03-17 20:33:56 -0700

    fixed sign in with google button, created a div with border instead of just the image

--- client/src/admin-login.js
+++ client/src/admin-login.js
@@ -1,15 +1,19 @@
 import adminLoginPic from './assets/admin-login-pic.png';
 import './admin-login.css';
-import admingooglesignin from './assets/googlesignin.png';
+import admingooglesignin from './assets/google-sign-in.png';
 
 export default function AdminLogin() {
    return(
     <div className = 'admin-component-container'>
         <img src={adminLoginPic} className = "admin-pic" alt = "admin-login"></img>
         <div className = 'admin-login-text-container'>
-            <div><h1>Login</h1>    
-            <p>Please sign in with an administrative account to <br></br> access additional features.</p>
-            <img src={admingooglesignin} className = "admin-google-sign-in" alt = "google-sign-in"></img></div>
+            <div>
+                <h1>Login</h1>    
+                <p>Please sign in with an administrative account to <br></br> access additional features.</p>
+                <div className = "google-sign-in-button-div">
+                    <img src={admingooglesignin} className = "admin-google-sign-in" alt = "google-sign-in"></img>
+                </div>
+            </div>
         </div>
     </div>
         
