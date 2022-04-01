0af4a6abbf235bade73be11d6d9b9a97000e5e3c
Gargi Deshpande <gargi@berkeley.edu>
2022-04-01 11:30:47 -0700

    styled left side text

--- client/src/pages/AppointmentTY.js
+++ client/src/pages/AppointmentTY.js
@@ -12,9 +12,9 @@ export default function AppointmentTY() {
                     <h1>Thank you!</h1>   
                     <p id="description-text">Thank you so much for scheduling an appointment with us; we look forward to seeing you!</p>
                     <p id="description-text">Your appointment is scheduled for</p>
-                    
+                    <p id="time-text">Wednesday, March 30th at 2:30 PM</p>
                     <p id="description-text">Please note down this time.</p>
-                    <p id="description-text">Before you visit, remember to take a look at our <a href="https://www.google.com/">FAQ for visiting the pantry.</a></p>
+                    <p id="description-text">Before you visit, remember to take a look at our <a id="weblink" href="https://www.google.com/">FAQ for visiting the pantry.</a></p>
                 
                 </div>
 
