2e4acbff6dda30c938b13fffde4c26bc8f30e47d
Gargi Deshpande <gargi@berkeley.edu>
2022-04-14 21:28:48 -0700

    completed model, get and post functions

---
+++ server/models/Appointment.js
@@ -0,0 +1,57 @@
+const { Sequelize, Model, DataTypes } = require("sequelize");
+
+class Appointment extends Model {}
+
+const initAppointmentModel = (sequelize) => {
+    Appointment.init({
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        date: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        time: {
+            type: DataTypes.STRING,
+            allowNull: false
+            
+        }, 
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        phone_number: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        visited: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        },
+        dietary_preferences: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        item_preferences: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }, 
+        notes: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+        
+    }, {
+        tableName: 'appointment',
+        modelName: 'Appointment',
+        sequelize
+    });
+}
+
+module.exports = { Appointment, initAppointmentModel };
+
--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,42 @@
-const { Food } = require("./models/Food")
+const { re } = require("prettier");
+const { Appointment } = require("./models/Appointment");
 
 module.exports = (app) => {
-    
+
+    app.get('/appointment', async (req, res) => {
+        const appointment = await Appointment.findAll();
+        res.status(200).json(appointment);
+    });
+
+    app.post('/appointment', async (req, res) => {
+        const last_name = req.body.last_name;
+        const first_name = req.body.first_name;
+        const date = req.body.date;
+        const time = req.body.time;
+        const email = req.body.email;
+        const phone_number = req.body.phone_number;
+        const visited = req.body.visited;
+        const dietary_preferences = req.body.dietary_preferences;
+        const item_preferences = req.body.item_preferences;
+        const notes = req.body.notes;
+        
+        console.log(req.body.last_name);
+
+        if (!last_name || last_name.length <= 0 ) return res.status(400).end();
+        if (!first_name || first_name.length <= 0) return res.status(400).end();
+        if (!date || date.length <= 0) return res.status(400).end();
+        if (!time || time.length <= 0) return res.status(400).end();
+        if (!email || email.length <= 0) return res.status(400).end();
+        if (!phone_number || phone_number.length < 10) return res.status(400).end();
+        if (visited == null) return res.status(400).end();
+
+        await Appointment.create({last_name, first_name, date, time, email, phone_number, visited, 
+            dietary_preferences, item_preferences, notes});
+        res.status(200).end();
+    });
+
+    app.put('/appointment', async (req, res) => {
+        
+    });
+
 }
