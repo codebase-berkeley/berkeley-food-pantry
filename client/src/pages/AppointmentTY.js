f44efe5c5b18bd367135f5e707ef4eddda951467
Gargi Deshpande <gargi@berkeley.edu>
2022-04-24 13:31:59 -0700

    fixed blue link, button issue

--- client/src/pages/AppointmentTY.js
+++ client/src/pages/AppointmentTY.js
@@ -1,11 +1,7 @@
 import circleImage from './../Images/thankyou.png';
 import { useLocation } from "react-router-dom";
-import AppointmentScheduler from './AppointmentScheduler';
 import './AppointmentTY.css';
-import UploadImageButton from '../components/UploadImageButton';
-import { Button } from 'bootstrap';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { ContactsOutlined } from '@mui/icons-material';
 
 export default function AppointmentTY(props) {
     const location = useLocation();
@@ -13,31 +9,29 @@ export default function AppointmentTY(props) {
     const timeSelected = location.state?.time;
 
    return(
-    <div className = 'full-page'>
+    <div className = 'thankyou-full-page'>
             
-            <div className = 'information-display'>
+            <div className = 'thankyou-information-display'>
 
-                <div className = 'confirmation-text'>
+                <div className = 'thankyou-confirmation-text'>
                     
                     <h1>Thank you!</h1>   
 
-                    <p id="description-text">Thank you so much for scheduling an appointment with us; we look forward to seeing you!</p>
-                    <p id="description-text">Your appointment is scheduled for</p>
-                    <p id="time-text">{dateSelected} at {timeSelected}</p>
+                    <p id="thankyou-description-text">Thank you so much for scheduling an appointment with us; we look forward to seeing you!</p>
+                    <p id="thankyou-description-text">Your appointment is scheduled for</p>
+                    <p id="thankyou-time-text">{dateSelected} at {timeSelected}</p>
                     
-                    <p id="description-text">Please note down this time.</p>
-                    <p id="description-text">Before you visit, remember to take a look at our <a id="weblink" href="https://www.berkeleyfoodpantry.org/needfood">FAQ for visiting the pantry.</a></p>
+                    <p id="thankyou-description-text">Please note down this time.</p>
+                    <p id="thankyou-description-text">Before you visit, remember to take a look at our <a id="thankyou-weblink" href="https://www.berkeleyfoodpantry.org/needfood">FAQ for visiting the pantry.</a></p>
                 
+                    <div className = 'thankyou-appt-button-container'>
+                        <Link to="/appointmentScheduler" className = "thankyou-appt-button" style ={{textDecoration: 'none'}}>Schedule New Appointment</Link>
+                    </div>
                 </div>
-
-                <div className = 'appt-button-container'>
-                    <Link to="/appointmentScheduler" className = "appt-button" style ={{textDecoration: 'none'}}>Schedule New Appointment</Link>
-                </div>
-
             </div>
             
             <div >
-                <img src={circleImage} className = "circle-pic" alt = "admin-login"></img>
+                <img src={circleImage} className = "thankyou-circle-pic" alt = "admin-login"></img>
             </div>
             
     </div>
