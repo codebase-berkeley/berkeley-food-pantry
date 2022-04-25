baea79d48d7275699d3776a2d2a34b1f23cf4cea
Abigail Brooks <a.brooks@berkeley.edu>
2022-04-24 20:05:05 -0700

    added Dropzone files

---
+++ client/src/components/Dropzone.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import PropTypes from 'prop-types';
+import './Dropzone.css';
+
+export default function Dropzone(props) {
+    
+}
--- client/src/components/UploadImageButton.js
+++ client/src/components/UploadImageButton.js
@@ -4,50 +4,9 @@ import saveitembutton from './../images/saveitembutton.png';
 import Select, { NonceProvider } from 'react-select';
 import makeAnimated from 'react-select/animated';
 import React, { useState, useCallback } from 'react';
-import Dropzone from 'react-dropzone';
-import {useDropzone} from 'react-dropzone'
 
 export default function UploadImageButton() {
     const [buttonType, setButtonType] = useState(false);
-
-    const Standard = () => {
-        // const getUploadParams = () => {
-        //   return { url: 'https://httpbin.org/post' }
-        // }
-      
-        // const handleChangeStatus = ({ meta }, status) => {
-        //   console.log(status, meta)
-        // }
-      
-        // const handleSubmit = (files, allFiles) => {
-        //   console.log(files.map(f => f.meta))
-        //   allFiles.forEach(f => f.remove())
-        // }
-      
-        return (
-          <Dropzone
-           
-          />
-        )
-      }
-
-      function MyDropzone() {
-        const onDrop = useCallback(acceptedFiles => {
-          // Do something with the files
-        }, [])
-        const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
-      
-        return (
-          <div {...getRootProps()}>
-            <input {...getInputProps()} />
-            {
-              isDragActive ?
-                <p>Drop the files here ...</p> :
-                <p>Drag 'n' drop some files here, or click to select files</p>
-            }
-          </div>
-        )
-      }
 
     function setButtonHelper() {
         setButtonType(true);
@@ -68,7 +27,6 @@ export default function UploadImageButton() {
         return (
             <div className = "upload-image-container">
             <div className = 'change-image-button'> 
-                {MyDropzone()}
                 <img src = {importimagecloud} className = 'cloud-format' alt = "upload image cloud"></img>
                 <h1 className = 'upload-image-text-h1'>Change Image</h1>
             </div>
