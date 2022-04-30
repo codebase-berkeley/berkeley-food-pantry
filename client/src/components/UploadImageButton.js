57e427149196c7bdf530b0a1ab2e296af68996d9
Abigail Brooks <a.brooks@berkeley.edu>
2022-04-30 15:29:30 -0700

    changed add img button

--- client/src/components/UploadImageButton.js
+++ client/src/components/UploadImageButton.js
@@ -1,18 +1,29 @@
 import importimagecloud from './../images/import-image-cloud.svg';
 import './../pages/AddFood.css';
+import './UploadImageButton.css'
 import React, { useState } from 'react';
 
 export default function UploadImageButton(props) {
     const [buttonType, setButtonType] = useState(false);
+    const hiddenFileInput = React.useRef(null);
 
     function setButtonHelper() {
         setButtonType(true)
+        hiddenFileInput.current.click()
     }
     
     if (buttonType == false) {
         return(
             <div className = "upload-image-container">
-            <input type = "file" accept="image/*" className = 'import-image-button' onChange={props.onSelectFile}/> 
+                <div className='import-image-button' role = 'button' onClick = {setButtonHelper}>
+                    <img src = {importimagecloud} className = 'cloud-format' alt = "upload image cloud"></img>
+                    <h1 className = 'upload-image-text-h1'>Import Image</h1>
+                </div>
+                <input type = "file" 
+                    accept="image/*" 
+                    ref={hiddenFileInput} 
+                    onChange={props.onSelectFile} 
+                    style ={{display: 'none'}}/> 
             </div>
         )
     } else if (buttonType == true) {
