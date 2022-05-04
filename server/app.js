91585450c121259ae4d6e1924eab5fa6cef96718
Ranon Larpcharern <ranonl@berkeley.edu>
2022-05-03 23:19:22 -0700

    fixed abbys dumb computer issue

--- server/app.js
+++ server/app.js
@@ -1,67 +1,24 @@
 const cors = require('cors');
 const express = require('express');
-const Session = require('express-session');
 const { Sequelize } = require('sequelize')
 const http = require('http');
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth')
-const cookieParser = require('cookie-parser');
 
 const routes = require('./routes');
 require('dotenv').config();
 const { initFoodModel } = require('./models/Food');
-const { initAppointmentModel } = require('./models/Appointment');
 
-const e = require('cors');
-
-const { PORT, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
+const PORT = process.env.PORT;
 
 const app = express();
 
-app.use(cors({ origin: ['http://localhost:3000'], credentials: true }));
-app.use(cookieParser());
+app.use(cors({ origin: ['http://localhost:3000'], credentials: true }))
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(express.json());
 
-app.use(Session({
-    cookie: { secure: false },
-    resave: false,
-    saveUninitialized: true,
-    secret: 'hello',
-    store: Session.MemoryStore(),
-    unset: 'destroy'
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use('admin-google', new GoogleStrategy.OAuth2Strategy({
-    clientID:     GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:4000/auth/google"
-  }, (accessToken, refreshToken, profile, done) => {
-    const email = profile.emails[0].value
-    if ( email.includes('@berkeley.edu')) {
-        return done(null, email);
-    } else {
-        return done(null, false);
-    }
-
-}))
-
-passport.serializeUser((email, done) => {
-    done(null, email)
-})
-
-passport.deserializeUser((email, done) => {
-    done(null, email)
-})
-
-console.log(process.env)
-const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING, {
-    dialect: 'postgres'
-});
+const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING);
 initFoodModel(sequelize);
-initAppointmentModel(sequelize);
 sequelize.sync();
 
 routes(app);
@@ -70,5 +27,3 @@ const httpServer = http.createServer(app);
 httpServer.listen(PORT, () => {
     console.log('Berkeley Food Pantry Server listening on port ' + PORT);
 });
-
-
--- server/routes.js
+++ server/routes.js
@@ -1,60 +1,111 @@
-const passport = require('passport')
 const { Food } = require("./models/Food")
+const AWS = require('aws-sdk');
+const { response } = require("express");
 
-const checkAuthenticated = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        next();
-    } else {
-        res.status(403).end();
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+
+/**
+ * @description Uploads an image to S3
+ * @param imageName Image name
+ * @param base64Image Image body converted to base 64
+ * @param type Image type
+ * @return string S3 image URL or error accordingly
+ */
+async function upload(imageName, base64Image, type) {
+    const params = {
+        Bucket: process.env.IMAGES_BUCKET,
+        Key: imageName,
+        Body: new Buffer.from(base64Image.replace(/^data:image\/\w+;base64,/, ""), 'base64'),
+        ContentType: type
+    };
+
+    let data;
+
+    try {
+        data = await promiseUpload(params);
+    } catch (err) {
+        console.error(err);
+
+        return "";
     }
+
+    return data.Location;
+}
+/**
+ * @description Promise an upload to S3
+ * @param params S3 bucket params
+ * @return data/err S3 response object
+ */
+function promiseUpload(params) {
+    return new Promise(function (resolve, reject) {
+        s3.upload(params, function (err, data) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
 }
 
 module.exports = (app) => {
-    app.get('/logout', (req, res) => {
-        req.logout();
-        res.redirect('http://localhost:3000/login');
-    })
-    app.get('/check_authenticated', checkAuthenticated, async (req, res) => {
-        res.status(200).end(); 
-    });
-
-    app.get('/food', async (req, res) => {
+    app.get('/food', async (reg, res) => {
         const food = await Food.findAll();
         res.status(200).json(food);
     });
     
-    app.delete('/food', checkAuthenticated, async (req, res) => {
+    app.delete('/food', async (req, res) => {
         const name = req.body.name;
         if (!name || name.length <= 0) return res.status(400).end();
         if (await Food.destroy({ where: { name }}) <= 0) return res.status(400).end();
         res.status(200).end();
-        
     });
 
-    app.post('/food', checkAuthenticated, async (req, res) => {
+    app.post('/food', async (req, res) => {
         const name = req.body.name;
+        console.log(req.body.data);
+        console.log(name);
         if (!name || name.length <= 0) return res.status(400).end();
-        const image = req.body.image_path;
         const instock = req.body.instock;
         const tags = req.body.tags;
+        const base64Image = req.body.image;
+        const imgName = req.body.image_name;
+
+        let response;
+        try {
+            response = await upload(imgName, base64Image);
+        } catch (err) {
+            console.error('Error uploading image: ', err.message);
+            return res.status(400).end()
+        }
         await Food.create({
-            
             name: name,
             instock: instock,
             tags: tags,
-            image_path: image
+            image_path: response
         });
         res.status(200).end();
     
     });
 
-    app.put('/food', checkAuthenticated, async (req, res) => {
+    app.put('/food', async (req, res) => {
         const name = req.body.name;
         if (!name || name.length <= 0) return res.status(400).end();
         const id = req.param.id; 
         const instock = req.body.instock;
         const tags = req.body.tags;
         const image = req.body.image_path;
+        console.log(req.body.name);
+         try {
+        response = await imagesService.upload(imageName, base64Image);
+    } catch (err) {
+        console.error('Error uploading image: ', err.message);
+        return next(new Error('Error uploading image: ', imageName));
+    }
         if (await Food.update({
             
             name: name, 
@@ -70,67 +121,4 @@ module.exports = (app) => {
         res.status(200).end();
         
     });
-
-    app.get('/appointment', async (req, res) => {
-        const appointment = await Appointment.findAll();
-        res.status(200).json(appointment);
-    });
-
-    app.post('/appointment', async (req, res) => {
-        const last_name = req.body.last_name;
-        const first_name = req.body.first_name;
-        const date = req.body.date;
-        const time = req.body.time;
-        const email = req.body.email;
-        const phone_number = req.body.phone_number;
-        const visited = req.body.visited;
-        const dietary_preferences = req.body.dietary_preferences;
-        const item_preferences = req.body.item_preferences;
-        const notes = req.body.notes;
-        
-        if (!last_name || last_name.length <= 0 ) return res.status(401).end();
-        if (!first_name || first_name.length <= 0) return res.status(402).end();
-        if (!date || date.length <= 0) return res.status(403).end();
-        if (!time || time.length <= 0) return res.status(404).end();
-        if (!email || email.length <= 0) return res.status(404).end();
-        if (!phone_number || phone_number.length < 10) return res.status(406).end();
-        if (visited == null) return res.status(407).end();
-
-        await Appointment.create({last_name: last_name, first_name: first_name, date: date, time: time, email: email, phone_number: phone_number, visited: visited, 
-            dietary_preferences: dietary_preferences, item_preferences:item_preferences, notes: notes});
-        res.status(200).end();
-    });
-
-    app.put('/appointment', async (req, res) => {
-        const apptID = req.body.id;
-        const visitedValue = req.body.visited;
-        
-        if (visitedValue == null) return res.status(400).end();
-
-        if (await Appointment.update(
-            {
-                visited: !visitedValue,
-            },
-            {
-                where: {id: apptID}
-            }
-        ) <= 0) return res.status(400).end();
-
-        res.status(200).end();
-    });
-
-    
-
-    const passportAuthenticate = passport.authenticate('admin-google', { 
-        scope: [ 'email', 'profile' ], 
-        prompt: 'select_account',
-        successRedirect: 'http://localhost:3000/edit-stock',
-        failureRedirect: 'http://localhost:3000/login'
-    })
-
-    app.get('/auth/google', (req, res, next) => {
-        next();
-    }, passportAuthenticate);
-
- 
 }
