64f617a18a28423cde45a2bb617b5a73d1665e14
Anthony Lu <anthonylu@berkeley.edu>
2022-05-05 23:25:52 -0700

    fixed google sign in button styling

--- client/src/pages/AdminLogin.js
+++ client/src/pages/AdminLogin.js
@@ -5,6 +5,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { LoginContext } from '../LoginContext';
 import AdminLoginNavbar from './AdminLoginNavbar';
+import googlesigninpic from './../images/googlesigninlogo.svg';
 
 
 export default function AdminLogin() {
@@ -40,8 +41,12 @@ export default function AdminLogin() {
                     <div>
                         <p>Please sign in with an administrative account to access additional features.</p>
                     </div>
-                    <a href="http://localhost:4000/auth/google">login to google!</a>
+                    <div className = "google-sign-in-button">
+                        <img src={googlesigninpic} className = "google-sign-in-logo" alt = "google sign in logo"></img>
+                        <a className = "google-sign-in-link" href="http://localhost:4000/auth/google">Sign in with Google</a>
+                    </div>
                 </div>
+
             </div>
         <Helmet>
             <title>Login</title>
